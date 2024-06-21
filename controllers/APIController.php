6aadaf250f90f4da3d2dd553dbdaf2996c6543a4
Jesús Gómez <jgomezbeltral88@gmail.com>
2024-06-21 19:14:06 +0200

    Añado configuracion cors al controlador de los mensajes

--- controllers/APIController.php
+++ controllers/APIController.php
@@ -1515,6 +1515,18 @@ class APIController {
         }
     
         $usuario_id = $_SESSION['id'];
+
+        // Configuración CORS
+        header('Access-Control-Allow-Origin: https://hijyusa.nyc.dom.my.id');
+        header('Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE');
+        header('Access-Control-Allow-Headers: Content-Type, Authorization');
+        header('Access-Control-Allow-Credentials: true');
+        
+        // Permitir solicitudes OPTIONS y responder con 200 OK si es necesario
+        if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+            http_response_code(200);
+            exit;
+        }
     
         // Obtener todos los mensajes desde la API externa
         // // Obtener el esquema (http o https)
